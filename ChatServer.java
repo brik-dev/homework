09fd87095717fb625f4965d366cf0e3513e920f9
Natalia Brik <natalia.brik@yahoo.com>
2020-10-09 01:11:12 +0200

    Основа домашней работы 4

---
+++ ChatServer.java
@@ -0,0 +1,11 @@
+package lesson_four.lesson;
+
+public class ChatServer {
+    public void start(int port){
+        System.out.println("Server started at: " + port);
+    }
+
+    public void stop(){
+        System.out.println("Server stopped.");
+    }
+}
