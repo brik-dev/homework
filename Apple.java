9575cca1c2e48d925e93dd50d05946b8b14c9f03
Natalia Brik <natalia.brik@yahoo.com>
2020-11-12 00:16:54 +0100

    clean

--- Apple.java
+++
@@ -1,9 +0,0 @@
-package lesson_1.homework;
-
-public class Apple extends Fruit {
-    public final int weight = 200;
-
-    public int getWeight() {
-        return weight;
-    }
-}
--- Orange.java
+++
@@ -1,9 +0,0 @@
-package lesson_1.homework;
-
-public class Orange extends Fruit {
-    static final int weight = 300;
-
-    public int getWeight() {
-        return weight;
-    }
-}
